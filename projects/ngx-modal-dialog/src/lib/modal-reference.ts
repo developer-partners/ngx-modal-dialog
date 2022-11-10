f843e4191ffae30862b910c986fc50e2e9826806
Hayk Shirinyan <haykshirinyan@gmail.com>
2022-11-10 04:22:45 -0800

    Added cancel detection event.

--- projects/ngx-modal-dialog/src/lib/modal-reference.ts
+++ projects/ngx-modal-dialog/src/lib/modal-reference.ts
@@ -4,16 +4,22 @@ import { ModalConfig } from './modal-config';
 
 export class ModalReference<TConfig, TResult = TConfig> {
   private _event: Subject<TResult>;
+  private _cancelEvent: Subject<void>;
 
   public config: ModalConfig<TConfig>;
 
   constructor(config: ModalConfig<TConfig>) {
     this.config = config;
     this._event = new Subject<TResult>();
+    this._cancelEvent = new Subject();
   }
 
   public result(): Observable<TResult> {
     return this._event.asObservable()
+  }
+
+  public cancelResult(): Observable<void> {
+    return this._cancelEvent.asObservable();
   }
 
   public closeSuccess(model?: TResult): void {
@@ -24,5 +30,8 @@ export class ModalReference<TConfig, TResult = TConfig> {
   public cancel(): void {
     // Just comlete the event transmission.
     this._event.complete();
+
+    this._cancelEvent.next();
+    this._cancelEvent.complete();
   }
 }
